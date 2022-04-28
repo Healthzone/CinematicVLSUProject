d43e81ef476a823d5855aa92f43bd78f5d5968a8
Healthzone <emelyanov-89@list.ru>
2022-04-28 18:55:44 +0300

    Импортировал TypeWriterPackage Продолжение на работой, создан черновой вариант sequence_02

---
+++ Assets/Scripts/MonitorTextTyper.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RedBlueGames.Tools.TextTyper;
+using UnityEngine.UI;
+using TMPro;
+
+/// <summary>
+/// Class that tests TextTyper and shows how to interface with it.
+/// </summary>
+[ExecuteAlways]
+public class MonitorTextTyper : MonoBehaviour
+{
+#pragma warning disable 0649 // Ignore "Field is never assigned to" warning, as these are assigned in inspector
+    [SerializeField]
+    private AudioClip printSoundEffect;
+
+    [Header("UI References")]
+
+    [SerializeField]
+    private float delayTimeCharacter;
+
+    [SerializeField]
+    private float delayBetweenLines;
+
+    private Queue<string> dialogueLines = new Queue<string>();
+
+    [SerializeField]
+    [Tooltip("The text typer element to test typing with")]
+    private TextTyper testTextTyper;
+
+
+#pragma warning restore 0649
+    public void StartTypingText()
+    {
+
+        Debug.Log("Start typing");
+        this.testTextTyper.CharacterPrinted.AddListener(this.HandleCharacterPrinted);
+        //testTextTyper.Skip();
+        dialogueLines.Clear();
+        dialogueLines.Enqueue("<delay = 0.1>...</delay>");
+        dialogueLines.Enqueue("<delay = 0.1>...</delay>");
+        dialogueLines.Enqueue("<delay = 0.1>...</delay>");
+        dialogueLines.Enqueue("<delay = 0.1>...</delay>");
+        dialogueLines.Enqueue("<delay = 0.1>...</delay>");
+        dialogueLines.Enqueue("<delay = 0.1>...</delay>");
+        dialogueLines.Enqueue("<delay = 0.1>...</delay>");
+        dialogueLines.Enqueue("<delay = 0.1>...</delay>");
+        dialogueLines.Enqueue("<delay = 0.1>...</delay>");
+        dialogueLines.Enqueue("<delay = 0.1>...</delay>");
+        dialogueLines.Enqueue("<delay = 0.1>...</delay>");
+        dialogueLines.Enqueue("<delay = 0.1>...</delay>");
+        dialogueLines.Enqueue("<delay = 0.1>...</delay>");
+        dialogueLines.Enqueue("Получен сигнал." +
+            "\nПеревод управления корабля искусственному интеллекту......." +
+            "\nИнициация протокола последний ковчег......." +
+            "\nПроверка систем корабля......." +
+            "\nСистемы корабля в удовлетворительном состоянии." +
+            "\nПроверка вооружения.......");
+        dialogueLines.Enqueue("Лазерный комплекс в норме.\nПулеметный комплекс в норме. \nРельсотрон в норме.");
+        dialogueLines.Enqueue("Запуск реактора............." +
+            "\nРеактор запущен.\nЗапуск маршевых двигателей. \nЗарядка сверхвсветового двигателя." +
+            "\nРасчет маршрута......." +
+            "\nКоличество сверхсветовых прыжков: 9");
+        StartCoroutine(DelayBetweenLines());
+    }
+
+    public void StartTypingTextCoroutine()
+    {
+        StartCoroutine(DelayBetweenLines());
+    }
+    
+    private IEnumerator DelayBetweenLines()
+    {
+        if (!testTextTyper.IsTyping)
+        {
+            ShowScript(delayTimeCharacter);
+        }
+        yield return new WaitForSeconds(delayBetweenLines);
+        yield return StartCoroutine(DelayBetweenLines());
+    }
+
+    
+    private void ShowScript(float delayTimesLines)
+    {
+        if (dialogueLines.Count <= 0)
+        {
+            return;
+        }
+
+        this.testTextTyper.TypeText(dialogueLines.Dequeue(), delayTimesLines);
+    }
+
+    private void HandleCharacterPrinted(string printedCharacter)
+    {
+        // Do not play a sound for whitespace
+        if (printedCharacter == " " || printedCharacter == "\n")
+        {
+            return;
+        }
+
+        var audioSource = this.GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = this.gameObject.AddComponent<AudioSource>();
+        }
+
+        audioSource.clip = this.printSoundEffect;
+        audioSource.Play();
+    }
+
+}
